b71b2dffdc4b6301eec5cea832a034b75ccc4255
mikey0-1 <mikebaliscao0@gmail.com>
2026-06-08 13:25:24 +0800

    added: abstract user and email admin login

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from tracker.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email' ,'username' ,'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser',)
+        })
+    )
+
+    search_fields = ('email', 'username','first_name', 'last_name')
+
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    username = models.CharField(max_length=50, null=True, blank=True, unique=False)
+    first_name = models.CharField(max_length=50, null=True, blank=True, unique=False)
+    last_name = models.CharField(max_length=50, null=True, blank=True, unique=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
