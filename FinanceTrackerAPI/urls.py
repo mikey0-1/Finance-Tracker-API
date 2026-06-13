3f44f3db8e2db149fe97d6a4003882793f41ff3e
mikey0-1 <mikebaliscao0@gmail.com>
2026-06-13 17:54:18 +0800

    moved profile related url to trackers/url

--- FinanceTrackerAPI/urls.py
+++ FinanceTrackerAPI/urls.py
@@ -28,9 +28,6 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/auth/register/', RegisterView.as_view(), name='register'),
-    path('api/auth/profile', ProfileView.as_view(), name='profile'),
-
     # tracker/urls.py
     path('api/', include('tracker.urls')),
 ]
